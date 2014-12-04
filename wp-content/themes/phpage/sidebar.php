3f52aca801c255108adc31bfdc143f60ce5d22b6
joao <joao@192.168.0.130>
2014-12-04 08:36:25 -0200

    Exibir About na barra lateral OK

--- wp-content/themes/phpage/sidebar.php
+++ wp-content/themes/phpage/sidebar.php
@@ -1,28 +1,31 @@
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
   <div class="sidebar-module sidebar-module-inset">
     <h4>About</h4>
-    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    <p><?php
+      $about = get_post(2);
+      echo $about->post_content;
+      ?></p>
   </div>
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">    
-      <?php 
+      <?php
       wp_get_archives(array('format' => 'custom'));
       ?>
-     </ol>
+    </ol>
   </div>
-  
+
   <div class="sidebar-module">
     <h4>Categories</h4>
     <ol class="list-unstyled">    
-      <?php 
+      <?php
       wp_list_categories(array('style' => 'list', 'title_li' => '', 'show_count' => true));
       ?>
-     </ol>
+    </ol>
   </div>
-  
-  
-  
+
+
+
   <div class="sidebar-module">
     <h4>Elsewhere</h4>
     <ol class="list-unstyled">
