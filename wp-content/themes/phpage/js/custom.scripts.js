9cea212c042a929fba9655745a44ff851be42492
joao <joao@192.168.0.130>
2014-12-05 09:29:59 -0200

    English menu OK

--- wp-content/themes/phpage/js/custom.scripts.js
+++ wp-content/themes/phpage/js/custom.scripts.js
@@ -6,13 +6,18 @@
 
 $(document).ready(function() {
 
+  // Contact form "bootstraping"
   $('.wpcf7 p').addClass('form-group');
-//  $('.wpcf7').text().wrap('<label></label>');
   $('.wpcf7 input, .wpcf7 textarea').addClass('form-control');
   if ($('.wpcf7 .screen-reader-response, .wpcf7 .wpcf7-response-output').html() !== '') {
     $('.wpcf7 .screen-reader-response, .wpcf7 .wpcf7-response-output').addClass('alert alert-info');
   }
-  console.log();
   $('.wpcf7 input[type=submit]').removeClass('form-control').addClass('btn btn-info');
+  
+  // English menu "Home" link fix 
+  if ($('.blog-nav li:last-child').text()==='PT'){
+    $('.blog-nav li:first-child a').attr('href', 'http://108.167.188.18/~phpag314/en/');
+  }
+  
 });
 
